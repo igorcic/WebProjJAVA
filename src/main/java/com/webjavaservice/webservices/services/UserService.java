b766c05d1e75ed9ed0f86a42d9a30816c2d59e92
igor bastos <igorbastoscic@gmail.com>
2022-08-17 00:25:15 -0300

    Exception findById

--- src/main/java/com/webjavaservice/webservices/services/UserService.java
+++ src/main/java/com/webjavaservice/webservices/services/UserService.java
@@ -2,6 +2,7 @@ package com.webjavaservice.webservices.services;
 
 import com.webjavaservice.webservices.entities.Users;
 import com.webjavaservice.webservices.repository.UserRepository;
+import com.webjavaservice.webservices.services.exception.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
@@ -24,7 +25,7 @@ public class UserService {
 
     public Users findById(Long id){
         Optional<Users> obj = userRepository.findById(id);
-        return obj.get();
+        return obj.orElseThrow(()-> new ResourceNotFoundException(id));
     }
 
     public Users insert(Users obj){
